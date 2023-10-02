a982273b654fd19c3ec963dc08f641a04575bc0d
PirraToZ <pirratoz@gmail.com>
2023-10-02 16:37:57 +0300

    feat(): algorithm for searching equivalent files

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -3,14 +3,13 @@
 //
 
 #include <experimental/filesystem>
-#include <QStringList>
-#include <QString>
-#include <QDebug>
 
 #include "Controller.h"
 #include "../utils/Dir.h"
 #include "../utils/File.h"
+#include "../md5/md5.h"
 
+using std::ifstream;
 
 namespace fs = std::experimental::filesystem;
 
@@ -24,6 +23,7 @@ void Controller::handle_folders(){
         if (!Dir::find_epath(path, epath)) continue;
         collect_files(path);
     }
+    this->start_comparing_files();
 }
 
 void Controller::collect_files(const string &path) {
@@ -57,4 +57,65 @@ Controller::Controller(QStringList &folders, QStringList &masks, QStringList &ep
     this->check_nested_folders = check_nested_folders;
     this->file_size_min = file_size_min;
     this->block_size = block_size;
+    this->block_memory = new char[block_size];
+}
+
+void Controller::start_comparing_files() {
+    for (auto & pair : this->files){
+        auto & paths = pair.second;
+        while (true) {
+            if (paths.empty() || paths.size() == 1) break;
+            paths = this->find_identity_content(pair.first, paths);
+        }
+        paths.clear();
+    }
+}
+
+list<string> Controller::find_identity_content(unsigned int size, list<string> paths) {
+    list<string> not_identical_files;
+    bool print_first_path = false;
+    const unsigned count_files = paths.size() - 1;
+    bool *equals = new bool[count_files];
+    memset(equals, true, count_files);
+
+    unsigned int count_iter = (size % this->block_size > 0) ?
+                              (size / this->block_size) + 1 : size / this->block_size;
+
+    string first_file_path = paths.front(); paths.pop_front();
+
+    for (int i = 0; i < count_iter; ++i) {
+        std::string hash = get_md5(first_file_path, i, size);
+        int j = 0;
+        for (auto path = paths.begin(); path != paths.end(); ++j, ++path){
+            if (!equals[j]) continue;
+            if (hash != get_md5(*path, i, size)) equals[j] = false;
+        }
+    }
+
+    for (int i = 0; i < count_files; ++i){
+        auto path = paths.front();
+        paths.pop_front();
+        if (equals[i]) qDebug() << path.c_str();
+        else not_identical_files.push_back(path);
+        print_first_path |= equals[i];
+    }
+    if (print_first_path) qDebug() << first_file_path.c_str() << "\n";
+
+    delete[] equals;
+    return not_identical_files;
+}
+
+string Controller::get_md5(string &path, int iter, unsigned int size) {
+    unsigned offset = iter * block_size;
+    memset(block_memory, 0, block_size);
+    ifstream file(path, std::ios::binary);
+    file.seekg(offset);
+    if (offset > size) file.read(block_memory, (size - offset));
+    else file.read(block_memory, block_size);
+    file.close();
+    return md5(block_memory);
+}
+
+Controller::~Controller() {
+    delete[] block_memory;
 }
--- controller/Controller.h
+++ controller/Controller.h
@@ -6,15 +6,21 @@
 #define COPYFINDER_CONTROLLER_H
 
 #include <QStringList>
+#include <QDebug>
+#include <QString>
+
 #include <string>
+#include <cstring>
 #include <queue>
 #include <list>
 #include <map>
+#include <fstream>
 
 using std::string;
 using std::queue;
 using std::list;
 using std::map;
+using std::memset;
 
 
 class Controller {
@@ -26,6 +32,8 @@ private:
     bool check_nested_folders;
     unsigned file_size_min;
     unsigned block_size;
+    char * block_memory;
+
 public:
     Controller(
             QStringList & folders,
@@ -35,9 +43,13 @@ public:
             unsigned file_size_min,
             unsigned block_size
             );
+    ~Controller();
 private:
     void add_found_file(const string & path, unsigned file_size);
     void collect_files(const string & path);
+    void start_comparing_files();
+    string get_md5(string & path, int iter, unsigned size);
+    list<string> find_identity_content(unsigned size, list<string> paths);
 public:
     void handle_folders();
 };
